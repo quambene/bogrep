9d454fce698b264adffb46da2f555ebe5be586ad
quambene <contact.quambene@gmail.com>
2023-09-18 01:44:28 +0200

    Test import

--- src/cmd/import.rs
+++ src/cmd/import.rs
@@ -11,22 +11,23 @@ pub fn import(config: &Config) -> Result<(), anyhow::Error> {
         .iter()
         .map(SourceReader::new)
         .collect::<Result<Vec<_>, anyhow::Error>>()?;
-    let target_bookmark_file = utils::open_file_in_write_mode(&config.target_bookmark_file)?;
+    let mut target_bookmark_file =
+        utils::open_file_in_read_write_mode(&config.target_bookmark_file)?;
 
-    import_bookmarks(source_reader, target_bookmark_file)?;
+    import_bookmarks(source_reader, &mut target_bookmark_file)?;
 
     Ok(())
 }
 
 fn import_bookmarks(
     mut source_reader: Vec<SourceReader>,
-    mut target_reader_writer: impl Read + Write,
+    target_reader_writer: &mut (impl Read + Write),
 ) -> Result<(), anyhow::Error> {
     let source_bookmarks = SourceBookmarks::read(source_reader.as_mut())?;
-    let mut target_bookmarks = TargetBookmarks::read(&mut target_reader_writer)?;
+    let mut target_bookmarks = TargetBookmarks::read(target_reader_writer)?;
 
     target_bookmarks.update(source_bookmarks)?;
-    target_bookmarks.write(&mut target_reader_writer)?;
+    target_bookmarks.write(target_reader_writer)?;
 
     log_import(&source_reader, &target_bookmarks);
 
@@ -51,4 +52,55 @@ fn log_import(source_reader: &[SourceReader], target_bookmarks: &TargetBookmarks
             .join(", ")
     );
     trace!("Imported bookmarks: {target_bookmarks:#?}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{json, Source};
+    use std::{collections::HashSet, io::Cursor, path::Path};
+
+    #[test]
+    fn test_import_bookmarks_google_chrome() {
+        let target_bookmarks = TargetBookmarks::default();
+        let target_bookmarks = json::serialize(&target_bookmarks).unwrap();
+        let path = Path::new("test_data/source/bookmarks_google-chrome.json");
+        let folders = vec![];
+        let source = Source::new(path, folders);
+        let source_reader = SourceReader::new(&source).unwrap();
+
+        let mut cursor = Cursor::new(Vec::new());
+        cursor.write_all(&target_bookmarks).unwrap();
+        let curser_after_read = cursor.position() as usize;
+        // Set cursor position to the start again to prepare cursor for reading.
+        cursor.set_position(0);
+
+        let res = import_bookmarks(vec![source_reader], &mut cursor);
+        assert!(res.is_ok(), "{}", res.unwrap_err());
+
+        let cursor_after_write = cursor.position() as usize;
+
+        let actual =
+            String::from_utf8(cursor.get_ref()[curser_after_read..cursor_after_write].to_vec())
+                .unwrap();
+        println!("actual: {actual}");
+        let actual_bookmarks = json::deserialize::<TargetBookmarks>(actual.as_bytes()).unwrap();
+        assert!(actual_bookmarks
+            .bookmarks
+            .iter()
+            .all(|bookmark| bookmark.last_cached == None));
+        assert_eq!(
+            actual_bookmarks
+            .bookmarks
+            .iter()
+            .map(|bookmark| bookmark.url.clone())
+            .collect::<HashSet<_>>(),
+            HashSet::from_iter([
+                String::from("https://www.deepl.com/translator"),
+                String::from("https://www.quantamagazine.org/how-mathematical-curves-power-cryptography-20220919/"),
+                String::from("https://en.wikipedia.org/wiki/Design_Patterns"),
+                String::from("https://doc.rust-lang.org/book/title-page.html"),
+            ])
+        );
+    }
 }
