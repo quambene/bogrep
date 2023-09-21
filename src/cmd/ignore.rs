017f28586f300a912d5f0aa086e961f77d54f1f9
quambene <contact.quambene@gmail.com>
2023-09-21 03:26:07 +0200

    Test ignore urls

--- src/cmd/ignore.rs
+++ src/cmd/ignore.rs
@@ -28,3 +28,55 @@ fn ignore_urls(
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{
+        fs::File,
+        io::{Cursor, Read},
+    };
+
+    #[test]
+    fn test_ignore_urls() {
+        let mut cursor = Cursor::new(Vec::new());
+        let mut settings = Settings::default();
+        let urls = vec![
+            String::from("https://youtube.com/"),
+            String::from("https://soundcloud.com/"),
+        ];
+
+        let res = ignore_urls(&mut settings, &urls, &mut cursor);
+        assert!(res.is_ok(), "{}", res.unwrap_err());
+        let actual_settings = String::from_utf8(cursor.into_inner()).unwrap();
+
+        let mut expected_settings = String::new();
+        let mut expected_file = File::open("test_data/ignore/settings.json").unwrap();
+        expected_file
+            .read_to_string(&mut expected_settings)
+            .unwrap();
+        assert_eq!(actual_settings, expected_settings);
+    }
+
+    #[test]
+    fn test_ignore_urls_duplicate() {
+        let mut cursor = Cursor::new(Vec::new());
+        let mut settings = Settings::default();
+        let urls = vec![
+            String::from("https://youtube.com/"),
+            String::from("https://youtube.com/"),
+            String::from("https://soundcloud.com/"),
+        ];
+
+        let res = ignore_urls(&mut settings, &urls, &mut cursor);
+        assert!(res.is_ok(), "{}", res.unwrap_err());
+        let actual_settings = String::from_utf8(cursor.into_inner()).unwrap();
+
+        let mut expected_settings = String::new();
+        let mut expected_file = File::open("test_data/ignore/settings.json").unwrap();
+        expected_file
+            .read_to_string(&mut expected_settings)
+            .unwrap();
+        assert_eq!(actual_settings, expected_settings);
+    }
+}
