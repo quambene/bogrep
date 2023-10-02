e88bbbe51fa4208ed7503e4f3a961f6182a1cb14
quambene <contact.quambene@gmail.com>
2023-10-03 01:25:10 +0200

    Test configure

--- tests/test_configure.rs
+++ tests/test_configure.rs
@@ -1,8 +1,17 @@
-use std::{env, process::Command};
+mod common;
+
+use std::{
+    env::{self},
+    path::Path,
+    process::Command,
+};
+use tempfile::tempdir;
 
 #[test]
 #[cfg_attr(not(feature = "integration-test"), ignore)]
 fn test_configure() {
+    let temp_dir = tempdir().unwrap();
+    let temp_path = temp_dir.path();
     let project_dir = env::var_os("CARGO_MANIFEST_DIR").unwrap();
     let source_path = format!(
         "{}/test_data/source/bookmarks_simple.txt",
@@ -10,9 +19,31 @@ fn test_configure() {
     );
 
     let mut cmd = Command::new("target/debug/bogrep");
-    cmd.env("BOGREP_HOME", "test_data/bogrep");
+    cmd.env("BOGREP_HOME", temp_path);
     cmd.args(["config", "--source", &source_path]);
 
     let res = cmd.output();
     assert!(res.is_ok(), "{}", res.unwrap_err());
+
+    let settings_path = temp_dir.path().join("settings.json");
+    assert!(settings_path.exists(), "{}", settings_path.display());
+
+    let bookmarks_path = temp_dir.path().join("bookmarks.json");
+    assert!(bookmarks_path.exists(), "{}", bookmarks_path.display());
+
+    let cache_path = temp_dir.path().join("cache");
+    assert!(cache_path.exists(), "{}", cache_path.display());
+
+    let (actual_settings, expected_settings) = common::compare_files(
+        &settings_path,
+        Path::new("test_data/configure/settings.json"),
+    );
+    let expected_settings = expected_settings.replace("path/to/bookmarks", &source_path);
+    assert_eq!(actual_settings, expected_settings);
+
+    let (actual_bookmarks, expected_bookmarks) = common::compare_files(
+        &bookmarks_path,
+        Path::new("test_data/target/bookmarks_empty.json"),
+    );
+    assert_eq!(actual_bookmarks, expected_bookmarks);
 }
