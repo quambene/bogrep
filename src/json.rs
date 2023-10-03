298358c68dbf237511d433b3782552ff5ed74693
quambene <contact.quambene@gmail.com>
2023-10-04 01:26:29 +0200

    Improve error message

--- src/json.rs
+++ src/json.rs
@@ -1,14 +1,17 @@
+use anyhow::Context;
 use serde::{de::DeserializeOwned, Serialize};
 
 pub fn serialize(value: impl Serialize) -> Result<Vec<u8>, anyhow::Error> {
     let mut buf = Vec::new();
     let formatter = serde_json::ser::PrettyFormatter::with_indent(b"    ");
     let mut serializer = serde_json::Serializer::with_formatter(&mut buf, formatter);
-    value.serialize(&mut serializer)?;
+    value
+        .serialize(&mut serializer)
+        .context("Can't serialize json")?;
     Ok(buf)
 }
 
 pub fn deserialize<T: DeserializeOwned>(slice: &[u8]) -> Result<T, anyhow::Error> {
-    let value = serde_json::from_slice(slice)?;
+    let value = serde_json::from_slice(slice).context("Can't deserialize json")?;
     Ok(value)
 }
