5521c9f5aa8f5c59e592f6d632280b8d738de8e7
quambene <contact.quambene@gmail.com>
2023-11-07 00:25:10 +0100

    Fix duplicate sources

--- src/settings.rs
+++ src/settings.rs
@@ -1,5 +1,5 @@
 use crate::{cache::CacheMode, json};
-use anyhow::Context;
+use anyhow::{anyhow, Context};
 use log::debug;
 use reqwest::Url;
 use serde::{Deserialize, Serialize};
@@ -103,8 +103,22 @@ impl Settings {
 
     pub fn set_source(&mut self, source: Source) -> Result<(), anyhow::Error> {
         debug!("Set source to {}", source.path.display());
-        self.sources.push(source);
-        Ok(())
+
+        let source_paths = self
+            .sources
+            .iter()
+            .map(|source| &source.path)
+            .collect::<Vec<_>>();
+
+        if !source_paths.contains(&&source.path) {
+            self.sources.push(source);
+            Ok(())
+        } else {
+            Err(anyhow!(format!(
+                "Source already configured: {}",
+                source.path.display()
+            )))
+        }
     }
 
     pub fn set_cache_mode(&mut self, cache_mode: Option<CacheMode>) {
@@ -202,6 +216,33 @@ mod tests {
                 ..Default::default()
             }
         );
+    }
+
+    #[test]
+    fn test_set_source_duplicate() {
+        let mut settings = Settings::default();
+        settings
+            .set_source(Source {
+                path: PathBuf::from("path/to/source"),
+                folders: vec![],
+            })
+            .unwrap();
+        assert_eq!(
+            settings,
+            Settings {
+                sources: vec![Source {
+                    path: PathBuf::from("path/to/source"),
+                    folders: vec![]
+                }],
+                ..Default::default()
+            }
+        );
+
+        let res = settings.set_source(Source {
+            path: PathBuf::from("path/to/source"),
+            folders: vec![],
+        });
+        assert!(res.is_err());
     }
 
     #[test]
