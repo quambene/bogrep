8a2d1288e54d38df72b1c77e73803091f68049da
quambene <contact.quambene@gmail.com>
2023-11-16 03:15:28 +0100

    Add test

--- tests/test_search.rs
+++ tests/test_search.rs
@@ -115,3 +115,57 @@ async fn test_search_case_insensitive() {
         .stdout("No matches in bookmarks\n")
         .stderr("");
 }
+
+#[tokio::test]
+#[cfg_attr(not(feature = "integration-test"), ignore)]
+async fn test_search_no_matched_lines() {
+    let mock_server = common::start_mock_server().await;
+    let mocks = common::mount_mocks(&mock_server, 3).await;
+    let temp_dir = tempdir().unwrap();
+    let temp_path = temp_dir.path();
+    assert!(temp_path.exists(), "Missing path: {}", temp_path.display());
+    let source_path = temp_path.join("test_data");
+    let source = &source_path.join("bookmarks_simple.txt");
+    fs::create_dir_all(&source_path).unwrap();
+    let mut file = File::create(source).unwrap();
+
+    for url in mocks.keys() {
+        writeln!(file, "{}", url).unwrap();
+    }
+
+    println!("Execute 'bogrep config --source {}'", source.display());
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    cmd.env("BOGREP_HOME", temp_path);
+    cmd.args(["config", "--source", source.to_str().unwrap()]);
+    cmd.output().unwrap();
+
+    println!("Execute 'bogrep import'");
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    cmd.env("BOGREP_HOME", temp_path);
+    cmd.args(["import"]);
+    cmd.output().unwrap();
+
+    println!("Execute 'bogrep fetch'");
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    cmd.env("BOGREP_HOME", temp_path);
+    cmd.args(["fetch"]);
+    cmd.output().unwrap();
+
+    println!("Execute 'bogrep -l \"Test content 1\"'");
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    cmd.env("BOGREP_HOME", temp_path);
+    cmd.args(["-l", "Test content 1"]);
+    cmd.assert()
+        .success()
+        .stdout(str::contains("Match in bookmark"))
+        .stderr("");
+
+    println!("Execute 'bogrep -l \"Test content 4\"'");
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    cmd.env("BOGREP_HOME", temp_path);
+    cmd.args(["-l", "Test content 4"]);
+    cmd.assert()
+        .success()
+        .stdout("No matches in bookmarks\n")
+        .stderr("");
+}
