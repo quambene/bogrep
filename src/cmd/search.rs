47da38913363aac8662235859e837cbbb9b95bb0
quambene <contact.quambene@gmail.com>
2023-11-28 01:46:15 +0100

    Fix panic

--- src/cmd/search.rs
+++ src/cmd/search.rs
@@ -100,9 +100,11 @@ fn find_matches(reader: impl BufRead, regex: &Regex) -> Result<Vec<String>, anyh
                 end_index = line.len();
             }
 
-            let truncated_line = &line[start_index..end_index];
-
-            matched_lines.push(truncated_line.to_owned());
+            if let Some(truncated_line) = line.get(start_index..end_index) {
+                matched_lines.push(truncated_line.to_owned());
+            } else {
+                matched_lines.push(line.to_owned());
+            }
         }
     }
 
