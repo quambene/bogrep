9ed8d8883298d5f075a419891c19acca5d48d207
quambene <contact.quambene@gmail.com>
2023-12-12 02:45:21 +0100

    Add tests

--- src/cmd/add.rs
+++ src/cmd/add.rs
@@ -69,9 +69,11 @@ mod tests {
 
     #[test]
     fn test_add_urls() {
+        let url1 = "https://url1.com";
+        let url2 = "https://url2.com";
         let mut expected_urls = HashSet::new();
-        expected_urls.insert("https://url1.com".to_owned());
-        expected_urls.insert("https://url2.com".to_owned());
+        expected_urls.insert(url1.to_owned());
+        expected_urls.insert(url2.to_owned());
 
         let target_bookmarks = TargetBookmarks::default();
         let bookmarks_json = JsonBookmarks::from(&target_bookmarks);
@@ -83,7 +85,73 @@ mod tests {
         target_reader.set_position(0);
         let mut target_writer = Cursor::new(Vec::new());
 
-        let urls = vec!["https://url1.com".to_owned(), "https://url2.com".to_owned()];
+        let urls = vec![url1.to_owned(), url2.to_owned()];
+
+        let res = add_urls(&urls, &mut target_reader, &mut target_writer);
+        assert!(res.is_ok(), "{}", res.unwrap_err());
+
+        let actual = target_writer.get_ref();
+        let actual_bookmarks = json::deserialize::<JsonBookmarks>(actual);
+        assert!(
+            actual_bookmarks.is_ok(),
+            "{}\n{}",
+            actual_bookmarks.unwrap_err(),
+            String::from_utf8(actual.to_owned()).unwrap()
+        );
+
+        let actual_bookmarks = actual_bookmarks.unwrap();
+        assert_eq!(actual_bookmarks.len(), 2);
+        assert!(
+            actual_bookmarks
+                .iter()
+                .all(|bookmark| bookmark.last_cached.is_none()
+                    && bookmark.sources.contains(&SourceType::Internal)),
+            "actual: {actual_bookmarks:#?}"
+        );
+        assert_eq!(
+            actual_bookmarks
+                .iter()
+                .map(|bookmark| bookmark.url.clone())
+                .collect::<HashSet<_>>(),
+            expected_urls,
+        );
+    }
+
+    #[test]
+    fn test_add_urls_existing() {
+        let url1 = "https://url1.com";
+        let url2 = "https://url2.com";
+        let mut expected_urls = HashSet::new();
+        expected_urls.insert(url1.to_owned());
+        expected_urls.insert(url2.to_owned());
+
+        let mut target_bookmarks = TargetBookmarks::default();
+        target_bookmarks.insert(TargetBookmark::new(
+            url1,
+            Utc::now(),
+            None,
+            HashSet::new(),
+            HashSet::new(),
+            Action::Add,
+        ));
+        target_bookmarks.insert(TargetBookmark::new(
+            url2,
+            Utc::now(),
+            None,
+            HashSet::new(),
+            HashSet::new(),
+            Action::Add,
+        ));
+        let bookmarks_json = JsonBookmarks::from(&target_bookmarks);
+        let buf = json::serialize(bookmarks_json).unwrap();
+
+        let mut target_reader: Cursor<Vec<u8>> = Cursor::new(Vec::new());
+        target_reader.write_all(&buf).unwrap();
+        // Set cursor position to the start again to prepare cursor for reading.
+        target_reader.set_position(0);
+        let mut target_writer = Cursor::new(Vec::new());
+
+        let urls = vec![url1.to_owned(), url2.to_owned()];
 
         let res = add_urls(&urls, &mut target_reader, &mut target_writer);
         assert!(res.is_ok(), "{}", res.unwrap_err());
