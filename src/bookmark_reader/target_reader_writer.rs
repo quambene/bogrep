30c63a6a1b56f8ceccb78e558e91581436b5e579
quambene <contact.quambene@gmail.com>
2024-01-29 02:38:27 +0100

    Fix test on windows

--- src/bookmark_reader/target_reader_writer.rs
+++ src/bookmark_reader/target_reader_writer.rs
@@ -1,8 +1,7 @@
 use super::{ReadTarget, WriteTarget};
 use crate::{errors::BogrepError, utils, TargetBookmarks};
-use log::debug;
 use std::{
-    fs::{self, File},
+    fs::File,
     path::{Path, PathBuf},
 };
 
@@ -45,27 +44,12 @@ impl TargetReaderWriter {
         self.writer.write(target_bookmarks)
     }
 
-    pub fn close(&self) -> Result<(), BogrepError> {
-        let from = &self.writer_path;
-        let to = &self.reader_path;
+    pub fn close(self) -> Result<(), BogrepError> {
+        let from = (self.writer, self.writer_path.as_path());
+        let to = (self.reader, self.reader_path.as_path());
 
-        debug!("Rename file from {} to {}", from.display(), to.display());
-        fs::rename(from, to).map_err(|err| BogrepError::RenameFile {
-            from: from.to_string_lossy().to_string(),
-            to: to.to_string_lossy().to_string(),
-            err,
-        })?;
+        utils::close_and_rename(from, to)?;
+
         Ok(())
-    }
-}
-
-impl Drop for TargetReaderWriter {
-    fn drop(&mut self) {
-        if self.writer_path.exists() {
-            // Clean up lock file.
-            if let Err(err) = fs::remove_file(&self.writer_path) {
-                eprintln!("Can't remove lock file: {err:?}")
-            }
-        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,34 @@
 use anyhow::anyhow;
 use bogrep::{cmd, Args, Config, Logger, Subcommands};
 use clap::Parser;
+use std::fs;
 use tokio::signal;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
+    let args = Args::parse();
+    Logger::init(args.verbose);
+    let config = Config::init()?;
+    let target_bookmark_lock_file = config.target_bookmark_lock_file.clone();
+
     tokio::select! {
         _ = signal::ctrl_c() => {
-            // Err for a graceful shutdown.
+            // Clean up lock file when aborting.
+            if target_bookmark_lock_file.exists() {
+                if let Err(err) = fs::remove_file(target_bookmark_lock_file) {
+                    eprintln!("Can't remove lock file: {err:?}")
+                }
+            }
+
             Err(anyhow!("Aborting ..."))
         },
-        res = run_app() => {
+        res = run_app(args, config) => {
             res
         }
     }
 }
 
-async fn run_app() -> Result<(), anyhow::Error> {
-    let args = Args::parse();
-    Logger::init(args.verbose);
-    let config = Config::init()?;
-
+async fn run_app(args: Args, config: Config) -> Result<(), anyhow::Error> {
     if let Some(subcommands) = args.subcommands {
         match subcommands {
             Subcommands::Config(args) => cmd::configure(config, args)?,
