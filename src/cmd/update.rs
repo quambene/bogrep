b2ed2653387de613096cdab3548b3b1582636493
quambene <contact.quambene@gmail.com>
2024-03-04 02:09:52 +0100

    Refactor update

--- src/cmd/update.rs
+++ src/cmd/update.rs
@@ -1,10 +1,11 @@
 use crate::{
     args::UpdateArgs,
-    bookmark_reader::{SourceReader, TargetReaderWriter},
-    bookmarks::{BookmarkProcessor, ProcessReport},
+    bookmark_reader::{ReadTarget, SourceReader, TargetReaderWriter, WriteTarget},
+    bookmarks::{BookmarkManager, BookmarkProcessor, ProcessReport},
     cache::CacheMode,
-    Action, Cache, Caching, Client, Config, Fetch, Settings, SourceBookmarks, TargetBookmarks,
+    Cache, Caching, Client, Config, Fetch, Settings, TargetBookmarks,
 };
+use chrono::Utc;
 use log::debug;
 
 /// Import the diff of source and target bookmarks. Fetch and cache websites for
@@ -38,13 +39,13 @@ pub async fn update(config: &Config, args: &UpdateArgs) -> Result<(), anyhow::Er
         &client,
         &cache,
         &mut source_readers,
-        &mut target_bookmarks,
+        &mut target_reader_writer.reader(),
+        &mut target_reader_writer.writer(),
         &config.settings,
         args.dry_run,
     )
     .await?;
 
-    target_reader_writer.write(&target_bookmarks)?;
     target_reader_writer.close()?;
 
     Ok(())
@@ -54,21 +55,20 @@ async fn update_bookmarks(
     client: &impl Fetch,
     cache: &impl Caching,
     source_readers: &mut [SourceReader],
-    target_bookmarks: &mut TargetBookmarks,
+    target_reader: &mut impl ReadTarget,
+    target_writer: &mut impl WriteTarget,
     settings: &Settings,
     dry_run: bool,
 ) -> Result<(), anyhow::Error> {
-    let mut source_bookmarks = SourceBookmarks::default();
+    let now = Utc::now();
+    let mut bookmark_manager = BookmarkManager::new(dry_run);
 
-    for source_reader in source_readers {
-        source_reader.import(&mut source_bookmarks)?;
-    }
+    target_reader.read(&mut bookmark_manager.target_bookmarks_mut())?;
 
-    target_bookmarks.update(&source_bookmarks)?;
-
-    if dry_run {
-        target_bookmarks.set_action(&Action::DryRun);
-    }
+    bookmark_manager.import(source_readers)?;
+    bookmark_manager.add_bookmarks(now)?;
+    bookmark_manager.remove_bookmarks();
+    bookmark_manager.set_actions();
 
     let bookmark_processor = BookmarkProcessor::new(
         client.clone(),
@@ -77,255 +77,21 @@ async fn update_bookmarks(
         ProcessReport::init(dry_run),
     );
     bookmark_processor
-        .process_bookmarks(target_bookmarks.values_mut().collect())
+        .process_bookmarks(
+            bookmark_manager
+                .target_bookmarks_mut()
+                .values_mut()
+                .collect(),
+        )
         .await?;
     bookmark_processor
-        .process_underlyings(target_bookmarks)
+        .process_underlyings(bookmark_manager.target_bookmarks_mut())
         .await?;
 
-    target_bookmarks.clean_up();
+    bookmark_manager.print_report(&source_readers);
+    bookmark_manager.finish();
+
+    target_writer.write(&bookmark_manager.target_bookmarks())?;
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::{bookmarks::RawSource, Action, MockCache, MockClient, TargetBookmark};
-    use chrono::Utc;
-    use std::{
-        collections::{HashMap, HashSet},
-        path::Path,
-    };
-    use url::Url;
-
-    #[tokio::test]
-    async fn test_update_bookmarks_mode_html() {
-        let now = Utc::now();
-        let client = MockClient::new();
-        let cache = MockCache::new(CacheMode::Html);
-        let bookmark_path = Path::new("test_data/bookmarks_chromium.json");
-        let source = RawSource::new(bookmark_path, vec![]);
-        let source_reader = SourceReader::init(&source).unwrap();
-        let settings = Settings::default();
-        let url1 = Url::parse("https://www.deepl.com/translator").unwrap();
-        let url2 = Url::parse(
-            "https://www.quantamagazine.org/how-mathematical-curves-power-cryptography-20220919/",
-        )
-        .unwrap();
-        let url3 = Url::parse("https://en.wikipedia.org/wiki/Design_Patterns").unwrap();
-        let url4 = Url::parse("https://doc.rust-lang.org/book/title-page.html").unwrap();
-        let expected_bookmarks: HashSet<Url> =
-            HashSet::from_iter([url1.clone(), url2.clone(), url3.clone(), url4.clone()]);
-        let mut target_bookmarks = TargetBookmarks::new(HashMap::from_iter([
-            (
-                url1.clone(),
-                TargetBookmark::builder_with_id(
-                    "dd30381b-8e67-4e84-9379-0852f60a7cd7".to_owned(),
-                    url1.clone(),
-                    now,
-                )
-                .with_action(Action::FetchAndAdd)
-                .add_cache_mode(CacheMode::Html)
-                .build(),
-            ),
-            (
-                url2.clone(),
-                TargetBookmark::builder_with_id(
-                    "25b6357e-6eda-4367-8212-84376c6efe05".to_owned(),
-                    url2.clone(),
-                    now,
-                )
-                .with_action(Action::FetchAndAdd)
-                .add_cache_mode(CacheMode::Html)
-                .build(),
-            ),
-        ]));
-        for url in &expected_bookmarks {
-            client
-                .add(
-                    "<html><head></head><body><img></img><p>Test content (fetched)</p></body></html>"
-                        .to_owned(),
-                    url,
-                )
-                .unwrap();
-        }
-        cache
-            .add(
-                "<html><head></head><body><p>Test content (already cached)</p></body></html>"
-                    .to_owned(),
-                target_bookmarks.get_mut(&url1).unwrap(),
-            )
-            .await
-            .unwrap();
-        cache
-            .add(
-                "<html><head></head><body><p>Test content (already cached)</p></body></html>"
-                    .to_owned(),
-                target_bookmarks.get_mut(&url2).unwrap(),
-            )
-            .await
-            .unwrap();
-
-        let res = update_bookmarks(
-            &client,
-            &cache,
-            &mut [source_reader],
-            &mut target_bookmarks,
-            &settings,
-            false,
-        )
-        .await;
-        assert!(res.is_ok());
-        assert_eq!(
-            target_bookmarks
-                .values()
-                .map(|bookmark| bookmark.url().clone())
-                .collect::<HashSet<_>>(),
-            HashSet::from_iter([url1.clone(), url2.clone(), url3.clone(), url4.clone()]),
-        );
-        assert_eq!(
-            cache
-                .cache_map()
-                .get("dd30381b-8e67-4e84-9379-0852f60a7cd7")
-                .unwrap(),
-            "<html><head></head><body><p>Test content (already cached)</p></body></html>"
-        );
-        assert_eq!(
-            cache
-                .cache_map()
-                .get("25b6357e-6eda-4367-8212-84376c6efe05")
-                .unwrap(),
-            "<html><head></head><body><p>Test content (already cached)</p></body></html>"
-        );
-        assert_eq!(
-            cache
-                .cache_map()
-                .get(target_bookmarks.get(&url3).unwrap().id())
-                .unwrap(),
-            "<html><head></head><body><p>Test content (fetched)</p></body></html>"
-        );
-        assert_eq!(
-            cache
-                .cache_map()
-                .get(target_bookmarks.get(&url4).unwrap().id())
-                .unwrap(),
-            "<html><head></head><body><p>Test content (fetched)</p></body></html>"
-        );
-    }
-
-    #[tokio::test]
-    async fn test_update_bookmarks_mode_text() {
-        let now = Utc::now();
-        let client = MockClient::new();
-        let cache = MockCache::new(CacheMode::Text);
-        let bookmark_path = Path::new("test_data/bookmarks_chromium.json");
-        let source = RawSource::new(bookmark_path, vec![]);
-        let source_reader = SourceReader::init(&source).unwrap();
-        let settings = Settings::default();
-        let url1 = Url::parse("https://www.deepl.com/translator").unwrap();
-        let url2 = Url::parse(
-            "https://www.quantamagazine.org/how-mathematical-curves-power-cryptography-20220919/",
-        )
-        .unwrap();
-        let url3 = Url::parse("https://en.wikipedia.org/wiki/Design_Patterns").unwrap();
-        let url4 = Url::parse("https://doc.rust-lang.org/book/title-page.html").unwrap();
-        let expected_bookmarks: HashSet<_> =
-            HashSet::from_iter([url1.clone(), url2.clone(), url3.clone(), url4.clone()]);
-        let mut target_bookmarks = TargetBookmarks::new(HashMap::from_iter([
-            (
-                url1.clone(),
-                TargetBookmark::builder_with_id(
-                    "dd30381b-8e67-4e84-9379-0852f60a7cd7".to_owned(),
-                    url1.clone(),
-                    now,
-                )
-                .with_action(Action::FetchAndAdd)
-                .add_cache_mode(CacheMode::Text)
-                .build(),
-            ),
-            (
-                url2.clone(),
-                TargetBookmark::builder_with_id(
-                    "25b6357e-6eda-4367-8212-84376c6efe05".to_owned(),
-                    url2.clone(),
-                    now,
-                )
-                .with_action(Action::FetchAndAdd)
-                .add_cache_mode(CacheMode::Text)
-                .build(),
-            ),
-        ]));
-        for url in &expected_bookmarks {
-            client
-                .add(
-                    "<html><head></head><body><img></img><p>Test content (fetched)</p></body></html>"
-                        .to_owned(),
-                    url,
-                )
-                .unwrap();
-        }
-        cache
-            .add(
-                "<html><head></head><body><p>Test content (already cached)</p></body></html>"
-                    .to_owned(),
-                target_bookmarks.get_mut(&url1).unwrap(),
-            )
-            .await
-            .unwrap();
-        cache
-            .add(
-                "<html><head></head><body><p>Test content (already cached)</p></body></html>"
-                    .to_owned(),
-                target_bookmarks.get_mut(&url2).unwrap(),
-            )
-            .await
-            .unwrap();
-
-        let res = update_bookmarks(
-            &client,
-            &cache,
-            &mut [source_reader],
-            &mut target_bookmarks,
-            &settings,
-            false,
-        )
-        .await;
-        assert!(res.is_ok());
-        assert_eq!(
-            target_bookmarks
-                .values()
-                .map(|bookmark| bookmark.url().clone())
-                .collect::<HashSet<_>>(),
-            HashSet::from_iter([url1.clone(), url2.clone(), url3.clone(), url4.clone()]),
-        );
-        assert_eq!(
-            cache
-                .cache_map()
-                .get("dd30381b-8e67-4e84-9379-0852f60a7cd7")
-                .unwrap(),
-            "Test content (already cached)"
-        );
-        assert_eq!(
-            cache
-                .cache_map()
-                .get("25b6357e-6eda-4367-8212-84376c6efe05")
-                .unwrap(),
-            "Test content (already cached)"
-        );
-        assert_eq!(
-            cache
-                .cache_map()
-                .get(target_bookmarks.get(&url3).unwrap().id())
-                .unwrap(),
-            "Test content (fetched)"
-        );
-        assert_eq!(
-            cache
-                .cache_map()
-                .get(target_bookmarks.get(&url4).unwrap().id())
-                .unwrap(),
-            "Test content (fetched)"
-        );
-    }
 }
